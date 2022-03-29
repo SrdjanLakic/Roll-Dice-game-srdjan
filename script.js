53409efd0cb17f47a0c838853f9fffd6807849aa
SrdjanLakic <srdjan949@gmail.com>
2022-03-29 18:43:21 -0400

    Initial commit

---
+++ script.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const score0El = document.getElementById('score--0');
+const score1El = document.getElementById('score--1');
+const diceEl = document.querySelector('.dice');
+const btnRoll = document.querySelector('.btn--roll');
+const btnNew = document.querySelector('.btn--new');
+const btnHold = document.querySelector('.btn--hold');
+const current0ElScore = document.getElementById('current--0');
+const current1ElScore = document.getElementById('current--1');
+
+score0El.textContent = 0;
+score1El.textContent = 0;
+diceEl.classList.add('hidden');
+let currentScore = 0;
+
+btnRoll.addEventListener('click', function () {
+  const dice = Math.trunc(Math.random() * 6) + 1;
+  diceEl.classList.remove('hidden');
+  diceEl.src = `dice-${dice}.png`;
+  if (dice !== 1) {
+    currentScore += dice;
+    current0ElScore.textContent = currentScore;
+  } else {
+  }
+});
